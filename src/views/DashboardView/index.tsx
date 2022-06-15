092a45bd4fe427fe5de505b2cdf33720a91349f2
KranzAklilu <kranz.aklilu@gmail.com>
2022-06-15 12:08:11 +0300

    removed profileView

--- src/views/DashboardView/index.tsx
+++ src/views/DashboardView/index.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect } from 'react';
 
 import { Container, Grid } from '@mui/material';
 import Page from '../../components/Page';
